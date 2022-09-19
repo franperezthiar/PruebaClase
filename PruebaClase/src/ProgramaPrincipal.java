0bf07395559789d741adb43a4467c5ca8222dd06
Fran Perez <franperezthiar@gmail.com>
2022-09-19 19:50:52 +0200

    Hola 3 por Hola 33

--- PruebaClase/src/ProgramaPrincipal.java
+++ PruebaClase/src/ProgramaPrincipal.java
@@ -4,7 +4,7 @@ public class ProgramaPrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
-		System.out.println("Hola3");
+		System.out.println("Hola33");
 	}
 
 }
